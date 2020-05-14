79e7c12dcb825de39d474f48bcf19231d70ee27d
Franck Allimant <fallimant@openstudio.fr>
2020-05-15 00:01:59 +0200

    Fix JS for entering dates in configuration form

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -5,13 +5,13 @@ return [
     'Pop-in images folder id' => 'Id du dossier des images de pop-in',
     'PopIn module configuration' => 'Configuration du module PopIn',
     'Pop-in campaigns' => 'Campagnes de pop-in',
-    'Start' => 'Début',
-    'End' => 'Fin',
+    'Start' => 'Début d\'affichage',
+    'End' => 'Fin d\'affichage',
     'Content' => 'Contenu',
     'This content will be displayed in the pop-in.' => 'Ce contenu sera affiché dans la pop-in.',
     'The first image in this content will be displayed in the pop-in.'
     => 'La première image de ce contenu sera affiché dans la pop-in.',
-    'This template will be displayed in the pop-in.' => 'Ce templa sera affiché dans la pop-in.',
+    'This template will be displayed in the pop-in.' => 'Ce template HTML sera affiché dans la pop-in.',
     'The pop-in.content hook will be called to display the pop-in. The content of this field will be available in the hook.'
     => 'Le hook pop-in.content sera appelé pour afficher la pop-in. Le contenu de ce champ sera disponible dans le hook.',
     'Add a campaign' => 'Ajouter une campagne',
