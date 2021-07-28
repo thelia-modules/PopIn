4519db4c870394acfe6561e4351135b0e57ed6d3
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2021-07-28 10:13:26 +0200

    Add FR translation

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -1,20 +1,23 @@
 <?php
 
-return [
-    'Pop-in images' => 'Images de pop-in',
-    'Pop-in images folder id' => 'Id du dossier des images de pop-in',
-    'PopIn module configuration' => 'Configuration du module PopIn',
-    'Pop-in campaigns' => 'Campagnes de pop-in',
-    'Start' => 'Début d\'affichage',
-    'End' => 'Fin d\'affichage',
-    'Content' => 'Contenu',
-    'This content will be displayed in the pop-in.' => 'Ce contenu sera affiché dans la pop-in.',
-    'The first image in this content will be displayed in the pop-in.'
-    => 'La première image de ce contenu sera affiché dans la pop-in.',
-    'This template will be displayed in the pop-in.' => 'Ce template HTML sera affiché dans la pop-in.',
-    'The pop-in.content hook will be called to display the pop-in. The content of this field will be available in the hook.'
-    => 'Le hook pop-in.content sera appelé pour afficher la pop-in. Le contenu de ce champ sera disponible dans le hook.',
+return array(
     'Add a campaign' => 'Ajouter une campagne',
-    'Edit this campaign' => 'Modifier cette campagne',
+    'Content' => 'Contenu',
     'Delete this campaign' => 'Supprimer cette campagne',
-];
+    'Description' => 'Description',
+    'Edit this campaign' => 'Modifier cette campagne',
+    'End' => 'Fin d\'affichage',
+    'Fill the fields below' => 'Remplissez les champs ci-dessous',
+    'Image' => 'Image',
+    'Link text' => 'Texte du lien',
+    'Link url' => 'Url du lien',
+    'Pop-in campaigns' => 'Campagnes de pop-in',
+    'PopIn module configuration' => 'Configuration du module PopIn',
+    'Postscriptum' => 'Postscriptum',
+    'Start' => 'Début d\'affichage',
+    'The first image in this content will be displayed in the pop-in.' => 'La première image de ce contenu sera affiché dans la pop-in.',
+    'The pop-in.content hook will be called to display the pop-in. The content of this field will be available in the hook.' => 'Le hook pop-in.content sera appelé pour afficher la pop-in. Le contenu de ce champ sera disponible dans le hook.',
+    'This content will be displayed in the pop-in.' => 'Ce contenu sera affiché dans la pop-in.',
+    'This template will be displayed in the pop-in.' => 'Ce template HTML sera affiché dans la pop-in.',
+    'Title' => 'Titre',
+);
---
+++ I18n/fr_FR.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'Content' => 'Contenu',
+    'Custom' => 'Personnalisé',
+    'Hook' => 'Hook',
+    'Image' => 'Image',
+    'Pop-in campaigns' => 'Campagne de Pop-in',
+    'Pop-in images' => 'Pop-in images',
+    'Pop-in images folder id' => 'Id du dossier d\'images pop in',
+    'Template' => 'Fichier',
+);
